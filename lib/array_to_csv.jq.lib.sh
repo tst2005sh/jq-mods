6b8a835c3ee8defa0375bd73665e239735bb0b9f
TsT <tst2005@gmail.com>
2021-03-05 20:12:38 +0100

    fix shell to jq function

--- lib/array_to_csv.jq.lib.sh
+++ lib/array_to_csv.jq.lib.sh
@@ -1,5 +1,8 @@
-jq_array_to_csv() { jq -r '.[]|@csv'; }
-jq_array_to_csv() {
-	jq_stack option -r
-	jq_stack call '.[]|@csv'
-}
+#jq_array_to_csv() { jq -r '.[]|@csv'; }
+#jq_array_to_csv() {
+#	jq_stack option -r
+#	jq_stack call '.[]|@csv'
+#}
+
+jq_function_array_to_csv='def array_to_csv: .[]|@csv;'
+ 
