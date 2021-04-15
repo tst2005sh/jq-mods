c91aa89428a8524e45fd036a7ae979b598a16123
TsT <tst2005@gmail.com>
2021-04-15 15:56:20 +0200

    improve test

--- demos/sqllike.sh
+++ demos/sqllike.sh
@@ -1,5 +1,8 @@
 . ./lib/jq16sqlbuiltin.jq.lib.sh
 
+
+
+result="$(
 jq -c -n "$jq_function_jq16sqlbuiltin"'{
     "assets":[
             {"assetid":1, "classid":1, "instanceid":5},
@@ -16,4 +19,11 @@ jq -c -n "$jq_function_jq16sqlbuiltin"'{
         [.classid,.instanceid]|tostring;
         {assetid:(.[0].assetid),name:(.[1].name)}
 )'
+)"
 
+expected='{"assetid":1,"name":"alpha"}
+{"assetid":2,"name":"alpha"}
+{"assetid":3,"name":"beta"}
+{"assetid":4,"name":"gamma"}'
+
+[ "$result" = "$expected" ] && echo ok || echo ko
