54e11abac4403d6b4634b9a397f66525e87c73be
TsT <tst2005@gmail.com>
2021-04-21 18:51:52 +0200

    assert_type improved

--- lib/assert_type.jq.lib.sh
+++ lib/assert_type.jq.lib.sh
@@ -3,8 +3,8 @@
 jq_function_assert_type='
 def assert_type($t):
 	if $t|type=="string" then
-		if type==$t then . else error("invalid type") end
+		if type==$t then . else error("assert_type: invalid type, expected \""+$t+"\", got \""+type+"\"") end
 	else
-		if type|is_in_array($t) then . else error("assert_type: invalid type") end
+		if type|is_in_array($t) then . else error("assert_type: invalid type, expected \""+($t|join("\" or \""))+"\", got \""+type+"\"") end
 	end
 ;'
