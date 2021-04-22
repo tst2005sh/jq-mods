8ce76823c13a8a4833575c72a69fa75f91d8bb27
TsT <tst2005@gmail.com>
2021-04-22 16:16:44 +0200

    vfrac

---
+++ lib/vfrac.jq.lib.sh
@@ -0,0 +1,48 @@
+jq_function_vfrac='
+# require: multiply is_in_array assert_type
+
+def vtype: assert_type("object")| .type?;
+def assert_vtype($t):
+	if $t|type=="string" then
+		if vtype==$t then . else error("invalid vtype") end
+	else
+		if vtype|is_in_array($t) then . else error("assert_type: invalid vtype") end
+	end
+;
+
+def vobj($t): {"type":$t, ($t): .};
+def vvalue: .[.type];
+def vnum: if type=="number" then [.] else . end|vobj("vnum");
+def vfrac: ({"up": .[0]|vnum, down:.[1]|vnum})|vobj("frac");
+
+def vfracsimplify:
+	assert_vtype("frac")|
+	(.frac.up|assert_vtype("vnum")|vvalue) as $up|
+	(.frac.down|assert_vtype("vnum")|vvalue) as $down|
+	.frac.up.vnum = ($up-$down)|
+	.frac.down.vnum = ($down-$up)
+;
+
+def vnumtonumber:
+	if type=="number" then . else
+		assert_vtype("vnum")|vvalue|multiply
+	end
+;
+
+def vfractonumber:
+	assert_vtype("frac")|vvalue|
+	(
+		.up|assert_vtype("vnum")|vnumtonumber
+	)/(
+		.down|assert_vtype("vnum")|vnumtonumber
+	)
+;
+
+def vfrac_insert($where;$by):
+	assert_vtype("frac")|
+	($by|vnumtonumber) as $b|
+	.frac[$where].vnum=[.frac[$where].vnum[],$b]
+;
+def vfrac_div($by): vfrac_insert("down";$by);
+def vfrac_mul($by): vfrac_insert("up";$by);
+'
