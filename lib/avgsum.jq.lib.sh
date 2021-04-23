134ff8265698c53f68297ee4f23f93933b2364f1
TsT <tst2005@gmail.com>
2021-04-23 12:44:43 +0200

    avgsum improved

--- lib/avgsum.jq.lib.sh
+++ lib/avgsum.jq.lib.sh
@@ -1,8 +1,9 @@
 jq_function_avgsum='
-def avgsum(f):
-	map(f)
-	| add as $sum
+def _avgsum:
+	add as $sum
 	| length as $count
-	| ($sum/$count*100|((if . < 0 then -1 else 1 end) as $sign | (.*$sign + 0.5) | floor * $sign)/100) as $avg
+	| ($sum/$count) as $avg
 	| {min:min, avg:$avg, max:max, sum:$sum, count:$count};
+def avgsum: _avgsum | .avg |= (.*100|((if . < 0 then -1 else 1 end) as $sign | (.*$sign + 0.5) | floor * $sign)/100) ;
+def avgsum(f): map(f) | avgsum ;
 '
