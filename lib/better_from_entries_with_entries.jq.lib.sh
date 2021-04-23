8fad7ecd8ce0026a41762f319f05afc67e265a0e
TsT <tst2005@gmail.com>
2021-04-23 17:09:20 +0200

    better_from_entries_with_entries

--- lib/better_from_entries_with_entries.jq.lib.sh
+++ lib/better_from_entries_with_entries.jq.lib.sh
@@ -1,7 +1,9 @@
-jq_function_pr2293='
+
+jq_function_better_from_entries_with_entries='
 def _from_entries_array: map(select(.key // .Key // .name // .Name >=0)) | map(if has("value") then .value elif has("Value") then .Value else empty end);
 def _from_entries_object: map({(.key // .Key // .name // .Name): (if has("value") then .value else .Value end)}) | add | .//={};
 def from_entries($type): if $type=="array" then _from_entries_array else _from_entries_object end;
 def from_entries: from_entries(if .[0].key|type=="number" then "array" else "object" end);
 def with_entries(f): type as $type | to_entries | map(f) | from_entries($type);
 '
+jq_function_pr2293="$jq_function_better_from_entries_with_entries"
