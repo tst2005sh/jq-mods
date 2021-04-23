21e0d3cfaabfd573eb6a5f1e528d28e771d49e60
TsT <tst2005@gmail.com>
2021-04-23 17:31:55 +0200

    assert_type

--- demos/assert_type.sh
+++ demos/assert_type.sh
@@ -4,5 +4,5 @@
 jq -ncM "$jq_function_is_in_array$jq_function_assert_type"'
 def test1:	["ok"]|assert_type(["array","number"])		| (.==["ok"]);
 def test2:	{"ok"}|assert_type("object")			| (.=={"ok"});
-[test1,test2]|all
+test1,test2
 '
