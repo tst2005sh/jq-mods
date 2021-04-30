83800fa501a184d0e4986305cd08a15cb59304d0
TsT <tst2005@gmail.com>
2021-04-30 18:03:16 +0200

    clean code for is_in_array

--- lib/is_in_array.jq.lib.sh
+++ lib/is_in_array.jq.lib.sh
@@ -1,4 +1,1 @@
-jq_function_is_in_array='
-def is_in_array($a): . as $x|$a|map(select(.==$x)!=null)|first//false;
-'
-#def is_in_array($a): length>(($a-[.])|length);
+jq_function_is_in_array='def is_in_array($a): any($a[] == .; .);'
