11334fff300657b1801c4285fff4abc5188d1c4d
TsT <tst2005@gmail.com>
2021-05-01 14:50:06 +0200

    keyrename improved

--- demos/keyrename.sh
+++ demos/keyrename.sh
@@ -2,3 +2,9 @@
 
 echo '{"a":"foo","b":"bar","buz":"buz"}' |
 jq -cM "$jq_function_keyrename"'keyrename("a";"foo")|keyrename("b";"bar")'
+
+echo '{"a":"foo","b":"bar","buz":"buz"}' |
+jq -cM "$jq_function_keyrename"'keyrename({"a":"foo","b":"bar","c":"ccc"})'
+
+echo '{"foo":"foo","bar":"bar","buz":"buz"}' |
+jq -cM "$jq_function_keyrename"'keyrename({"a":"foo","b":"bar","c":"ccc"})'
--- lib/keyrename.jq.lib.sh
+++ lib/keyrename.jq.lib.sh
@@ -1,5 +1,9 @@
 
 #jq_function_keyrename='def keyrename($from;$to): .+([{"key": $to, "value": .[$from]}]|from_entries)|del(.[$from]);'
 #jq_function_keyrename='def keyrename($from;$to): to_entries|map(.key |= if .==$from then ($to) else . end)|from_entries;'
-jq_function_keyrename='def keyrename($from;$to): with_entries(.key |= if .==$from then ($to) else . end);'
+#jq_function_keyrename='def keyrename($from;$to): with_entries(.key |= if .==$from then ($to) else . end);'
+jq_function_keyrename='
+def keyrename($o): with_entries(.key |= ($o[.] //.) );
+def keyrename($from;$to): keyrename({($from):$to});
+'
 #deps:none
