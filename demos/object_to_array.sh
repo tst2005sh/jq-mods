4e0d6d967de9981711d4630aebfad7692b1b03fb
TsT <tst2005@gmail.com>
2021-07-05 12:43:48 +0200

    MAJOR behavior changed: object_to_array(...)

--- demos/object_to_array.sh
+++ demos/object_to_array.sh
@@ -1,11 +1,11 @@
 
 # syntax:
 # 'object_to_array' will produce a content with all object keys as column
-# 'object_to_array(["key1","key2"])' will produce a content with only specified object keys (key1 and key2)
-# 'object_to_array(["key1","key2"];"*") will produce a content with all object keys as column but in specified order : key1 then key2 then all others (in not specified order)
+# '_object_to_array(["key1","key2"])' will produce a content with only specified object keys (key1 and key2)
+# 'object_to_array(["key1","key2"]) will produce a content with all object keys as column but in specified order : key1 then key2 then all others (in not specified order)
 
 . ./lib/object_to_array.jq.lib.sh
-. ./lib/array_to_csv.jq.lib.sh
+#. ./lib/tocsv.jq.lib.sh
 
 sample1() {
 echo '[
@@ -22,7 +22,7 @@ echo '[
 ####
 result="$(
 	sample1 |
-	jq -cM "$jq_function_object_to_array"'object_to_array(["BAR"])'
+	jq -cM "$jq_function_object_to_array"'map(with_entries(select(.key=="BAR")))|object_to_array(["BAR"])'
 )"
 expected='[["BAR"],["bar1"],["bar2"]]'
 [ "$result" = "$expected" ] && echo ok || echo ko
@@ -34,21 +34,21 @@ expected='[["BAR","BUZ","FOO"],["bar1","zzz1","foo1"],["bar2",null,"foo2"]]'
 [ "$result" = "$expected" ] && echo ok || echo ko
 
 ####
-result="$(
-	sample1 |
-	jq -r "$jq_function_object_to_array$jq_function_array_to_csv"'object_to_array|array_to_csv'
-)"
-
-expected='"BAR","BUZ","FOO"
-"bar1","zzz1","foo1"
-"bar2",,"foo2"'
-[ "$result" = "$expected" ] && echo ok || echo ko
+#result="$(
+#	sample1 |
+#	jq -r "$jq_function_object_to_array$jq_function_array_to_csv"'object_to_array|array_to_csv'
+#)"
+#
+#expected='"BAR","BUZ","FOO"
+#"bar1","zzz1","foo1"
+#"bar2",,"foo2"'
+#[ "$result" = "$expected" ] && echo ok || echo ko
 
 
 ####
 result="$(
 	sample1 |	
-	jq -cM "$jq_function_object_to_array"'object_to_array(["BUZ"];"*")'
+	jq -cM "$jq_function_object_to_array"'object_to_array(["BUZ"])'
 )"
 expected='[["BUZ","BAR","FOO"],["zzz1","bar1","foo1"],[null,"bar2","foo2"]]'
 [ "$result" = "$expected" ] && echo ok || echo ko
@@ -57,7 +57,7 @@ expected='[["BUZ","BAR","FOO"],["zzz1","bar1","foo1"],[null,"bar2","foo2"]]'
 
 result="$(
 	sample1 |	
-	jq -cM "$jq_function_object_to_array"'object_to_array(["BUZ"];null)'
+	jq -cM "$jq_function_object_to_array"'map({"BUZ":.BUZ})|object_to_array'
 )"
 expected='[["BUZ"],["zzz1"],[null]]'
 [ "$result" = "$expected" ] && echo ok || echo ko
--- lib/object_to_array.jq.lib.sh
+++ lib/object_to_array.jq.lib.sh
@@ -1,23 +1,9 @@
-# my version
-#jq_function_object_to_array='def object_to_array:
-#	{
-#		"data": .,
-#		"keys": (.|map(keys)|flatten(1)|unique)
-#	} |
-#	.data |= map(
-#		to_entries|sort_by(.key)|map(.value)|flatten(1)
-#	) |
-#	[ [.keys|sort], .data ] | flatten(1);
-#'
 
 jq_deps_object_to_array=''
-# thanks to geirha #jq 20180601
 jq_function_object_to_array='
-def object_to_array($k): [$k,(.[]|[ .[$k[]] ])];
-def object_to_array: object_to_array([.[]|keys[]]|unique);
-def object_to_array($k;$z): if $z!="*" then object_to_array($k) else
-	([.[]|keys[]]|unique) as $a| object_to_array([$k, ($a-$k)]|add)
-end;
+def _object_to_array($k): [$k,(.[]|[ .[$k[]] ])];
+def object_to_array: _object_to_array([.[]|keys[]]|unique);
+def object_to_array($k): ([.[]|keys[]]|unique) as $a| _object_to_array([$k, ($a-$k)]|add);
 '
-#def object_to_array: ([.[]|keys[]]|unique) as $k | [$k,(.[]|[ .[$k[]] ])];
+# thanks to geirha #jq 20180601
 
