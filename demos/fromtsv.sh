59773d47d908c30092887b4ca3fba58fc0f74fce
TsT <tst2005@gmail.com>
2021-07-13 23:32:22 +0200

    fromtsv

--- demos/fromtsv.sh
+++ demos/fromtsv.sh
@@ -1,26 +1,25 @@
 . ./lib/fromtsv.jq.lib.sh
 
-
-# data-samples/sample1a.tsv
-expected="$(
-cat<<EOF
-X	XX	XXX	XXXX
-a'a	"b""b"	"""c'c"""	"""d""d"""
-EOF
-)"
-
 #         [["X","XX","XXX","XXXX"],["a'a","b\"b","\"c'c\"","\"d\"d\""]]
 expected='[["X","XX","XXX","XXXX"],["a'\''a","b\"b","\"c'\''c\"","\"d\"d\""]]'
 
 result="$(
 	cat data-samples/sample1a.tsv |
-	jq -RcMr "$jq_function_fromtsv$jq_function_totsv"' [.,inputs]|fromtsv'
+	jq -RcMr "$jq_function_fromtsv"' [.,inputs]|fromtsv'
 )"
 [ "$result" = "$expected" ] && echo ok || echo ko
 
 result="$(
 	cat data-samples/sample1b.tsv |
-	jq -RcMr "$jq_function_fromtsv$jq_function_totsv"' [.,inputs]|fromtsv'
+	jq -RcMr "$jq_function_fromtsv"' [.,inputs]|fromtsv'
 )"
 [ "$result" = "$expected" ] && echo ok || echo ko
 
+result="$(
+	printf 'foo\tbar\n"x\\ny"\tBAR\n' |
+	jq -RcMr "$jq_function_fromtsv"'
+		[.,inputs]|fromtsv == [["foo","bar"],["x\ny","BAR"]]
+	'
+)"
+expected='true'
+[ "$result" = "$expected" ] && echo ok || echo ko
