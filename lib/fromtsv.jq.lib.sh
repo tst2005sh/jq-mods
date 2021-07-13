d56705e3e2daa0ceae02e1631a7d6f2ddab97f0c
TsT <tst2005@gmail.com>
2021-07-13 19:16:40 +0200

    fromtsv final bugfix

--- lib/fromtsv.jq.lib.sh
+++ lib/fromtsv.jq.lib.sh
@@ -2,11 +2,11 @@
 jq_deps_fromtsv=''
 jq_function_fromtsv='def fromtsv:
 	map(split("\t")|
-	map(if ((startswith("\"")) and (endswith("\""))) then
-		.[1:-1]|
-		gsub("\"\"";"\"")|
+	map(
+		if ((startswith("\"")) and (endswith("\""))) then
+			.[1:-1]|gsub("\"\"";"\"")
+		else	. end|
 		gsub("\\\\n";"\n")|gsub("\\\\r";"\r")|
 		gsub("\\\\t";"\t")|gsub("\\\\";"\\")
-	else .
-	end))
+	))
 ;'
