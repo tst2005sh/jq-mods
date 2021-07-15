637d589f7215e00e29c66d03a34475b310b4fe17
TsT <tst2005@gmail.com>
2021-07-15 16:32:33 +0200

    tsv/json_array: performance improvment try

--- lib/fromtsv.jq.lib.sh
+++ lib/fromtsv.jq.lib.sh
@@ -10,3 +10,15 @@ jq_function_fromtsv='def fromtsv:
 		gsub("\\\\t";"\t")|gsub("\\\\";"\\")
 	))
 ;'
+jq_function_fromtsv='def fromtsv:
+	map(
+		gsub("\\\\n";"\n")|gsub("\\\\r";"\r")|split("\t")|
+		map(
+			if ((startswith("\"")) and (endswith("\""))) then
+				.[1:-1]|gsub("\"\"";"\"")
+			else	. end
+			|gsub("\\\\t";"\t")
+			|gsub("\\\\";"\\")
+		)
+	)
+;'
