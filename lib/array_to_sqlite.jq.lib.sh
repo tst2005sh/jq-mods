639799a5b7995d27988dfa73a1993bf916182328
TsT <tst2005@gmail.com>
2021-07-17 00:52:11 +0200

    first json to sqlite support

---
+++ lib/array_to_sqlite.jq.lib.sh
@@ -0,0 +1,26 @@
+
+jq_deps_array_to_sqlite=''
+jq_function_array_to_sqlite='
+def ifquote(q): if (type=="null") then "NULL" elif (type=="string") then q else . end;
+def sql_squote: "'\''\( gsub("'\''";"'\'\''") )'\''";
+def sql_dquote: "\"\( gsub("\"";"\"\"") )\"";
+def sql_tickquote: "`\(.)`";
+def sql_brackquote: "[\(.)]";
+def array_to_sqlite:
+	("data") as $tablename|
+	"PRAGMA foreign_keys=OFF;",
+	"BEGIN TRANSACTION;",
+	"CREATE TABLE \($tablename|sql_dquote) ( \(
+		first |
+			[ (first|"\(sql_tickquote) text PRIMARY KEY"), (.[1:][]|"\(sql_tickquote) text")]
+		|join(", ")
+	) );",
+	(
+		.[1:]|map(
+		"INSERT INTO \($tablename|sql_dquote) VALUES( \(
+			map(ifquote(sql_squote))|join(", ")
+		) );"
+	))[],
+	"COMMIT;"
+
+;'
