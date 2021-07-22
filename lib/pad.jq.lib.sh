3733840bd864bd733dbc60c9b8dc600059d868a4
TsT <tst2005@gmail.com>
2021-07-22 15:57:37 +0200

    another lpad way

--- lib/pad.jq.lib.sh
+++ lib/pad.jq.lib.sh
@@ -7,3 +7,6 @@ jq_function_pad='def pad($n): " "*([0,($n-length)]|max);'
 jq_function_rpad='def lpad($n): pad($n)+.;'
 jq_function_lpad='def rpad($n): .+pad($n);'
 #echo '"1234567890"' | jq "$jq_function_pad""$jq_function_rpad""$jq_function_lpad"' rpad(10) '; exit
+
+#22:03:44 <    geirha> every language needs a leftpad library
+#def leftpad(n): if length > n then . else (" "*n + .)[-n:] end; "12345678901", "123" | leftpad(10)'
