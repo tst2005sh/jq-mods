5a8d65c4e09c1581daef9c30061ecaf54c0d48d1
TsT <tst2005@gmail.com>
2021-09-02 18:41:15 +0200

    tobase: bugfix small performance improvement

--- lib/tobase.jq.lib.sh
+++ lib/tobase.jq.lib.sh
@@ -1,4 +1,4 @@
 
 jq_deps_tobase=''
 # source: https://github.com/stedolan/jq/pull/2241
-jq_function_tobase='def tobase($a): [while(. > 0; (. / $a) | floor) % $a]|if length==0 then [0] else . end;'
+jq_function_tobase='def tobase($a): [while(. > 0; (. / $a) | floor) % $a]|if .==[] then [0] else . end;'
