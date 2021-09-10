81bd10ac21cba31e0170410222a83ca790ae365f
TsT <tst2005@gmail.com>
2021-09-10 12:35:21 +0200

    unsortedunique improvement

--- demos/unsortedunique.sh
+++ demos/unsortedunique.sh
@@ -4,3 +4,15 @@ echo '["foo","bar","foo","bar"]' |
 jq -cM "$jq_function_unsortedunique"'
 	unsortedunique == ["foo","bar"]
 '
+
+echo '["foo","bar","buz","foo","bar"]' |
+jq -cM "$jq_function_unsortedunique"'
+	unsortedunique == ["foo","bar","buz"]
+'
+
+echo '["foo","bar","foo","bar","buz"]' |
+jq -cM "$jq_function_unsortedunique"'
+	unsortedunique == ["foo","bar","buz"]
+'
+
+
