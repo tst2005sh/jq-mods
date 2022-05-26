3bc0bdf3918610a99e0d07345239812745ca69c6
TsT <tst2005@gmail.com>
2022-05-26 14:56:35 +0200

    json_array/sqlite_sql: auto-detect type (support: string=TEXT, number=NUMERIC, boolean=BOOLEAN); disable first field as primary key

--- lib/array_to_sqlite.jq.lib.sh
+++ lib/array_to_sqlite.jq.lib.sh
@@ -6,21 +6,24 @@ def sql_squote: "'\''\( gsub("'\''";"'\'\''") )'\''";
 def sql_dquote: "\"\( gsub("\"";"\"\"") )\"";
 def sql_tickquote: "`\(.)`";
 def sql_brackquote: "[\(.)]";
-def array_to_sqlite($tablename):
+def sql_type: ({"string":"TEXT","number":"NUMERIC","boolean":"BOOLEAN"})[type] // ("unknown sql type for json type \(type)"|error);
+def schema_first_field_is_primary_key: [ (.[0]|"\(.[0]) \(.[1]) PRIMARY KEY"), (.[1:][]|"\(.[0]) \(.[1])") ];
+def schema_no_primary_key: map( "\(.[0]) \(.[1])" );
+def array_to_sqlite($tablename;primary):
 	"PRAGMA foreign_keys=OFF;",
 	"BEGIN TRANSACTION;",
 	"CREATE TABLE \($tablename|sql_dquote) ( \(
-		first |
-			[ (first|"\(sql_tickquote) text PRIMARY KEY"), (.[1:][]|"\(sql_tickquote) text")]
-		|join(", ")
+		[(.[0]|map(sql_tickquote)),(.[1]|map(sql_type))]|transpose
+		| primary
+		| join(", ")
 	) );",
 	(
 		.[1:]|map(
 		"INSERT INTO \($tablename|sql_dquote) VALUES( \(
-			map(ifquote(sql_squote))|join(", ")
+			map(ifquote(sql_squote)|tostring)|join(", ")
 		) );"
 	))[],
 	"COMMIT;"
 ;
-def array_to_sqlite: array_to_sqlite("data");
+def array_to_sqlite: array_to_sqlite("data";schema_no_primary_key);
 '
