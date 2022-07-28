b0b4bf7c414701e27e0e3fe7f69a5879be2dca70
TsT <tst2005@gmail.com>
2022-07-28 16:01:55 +0200

    unsortedunique renamed to unsorted_unique

---
+++ lib/unsorted_unique.jq.lib.sh
@@ -0,0 +1,7 @@
+
+jq_deps_unsorted_unique=''
+#jq_function_unsorted_unique='def unsorted_unique: to_entries|group_by(.value)|map(min_by(.key))|sort_by(.key)|map(.value);'
+#jq_function_unsorted_unique='def unsorted_unique: to_entries|unique_by(.value)|sort_by(.key)|map(.value);'
+# Thanks geirha (2021-09-10 12:12:16)
+jq_function_unsorted_unique="$(cat "${dir:-.}/jq/unsorted_unique.jq")"
+
--- lib/unsortedunique.jq.lib.sh
+++
@@ -1,5 +0,0 @@
-
-jq_deps_unsortedunique=''
-#jq_function_unsortedunique='def unsortedunique: to_entries|group_by(.value)|map(min_by(.key))|sort_by(.key)|map(.value);'
-jq_function_unsortedunique='def unsortedunique: to_entries|unique_by(.value)|sort_by(.key)|map(.value);'
-# Thanks geirha (2021-09-10 12:12:16)
