dd0d91afe28c8f284b25fedb9e5be562d22c3f86
TsT <tst2005@gmail.com>
2022-07-29 15:12:55 +0200

    unflatify

--- lib/unflatify.jq.lib.sh
+++ lib/unflatify.jq.lib.sh
@@ -3,4 +3,5 @@ jq_deps_unflatify=''
 jq_option_unflatify=''
 #jq_function_unflatify='def unflatify(f;$k): to_entries | group_by(.key | . != scan($k)[]) | .[-1][] |= (.key |= scan($k)[]) | map(from_entries) | .[-1] as $o | .[0] | f = $o;'
 jq_function_unflatify='def unflatify(f;$k): to_entries|group_by(.key|.!= scan($k)[]) | if length==1 then first|from_entries else .[-1][]|=(.key|=scan($k)[])|map(from_entries) |.[-1] as $o |.[0] | f=$o end;'
+jq_function_unflatify="$(cat "${dir:-.}/jq/unflatify.jq")"
 # thanks emanuele6 !
