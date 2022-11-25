a481c61cba120991913cddf18fd3658a1342d0c8
TsT <tst2005@gmail.com>
2022-11-25 15:01:22 +0100

    is_in_array

--- lib/is_in_array.jq.lib.sh
+++ lib/is_in_array.jq.lib.sh
@@ -1,4 +1,4 @@
 jq_deps_is_in_array=''
 jq_function_is_in_array='def is_in_array($a): any($a[] == .; .);'
-
+jq_function_is_in_array="$(cat "${dir:-.}/jq/is_in_array.jq")"
 # Note: is_in_array($myarray) equals IN($myarray[]) (IN function is available in jq 1.6)
