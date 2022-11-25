4f00d08e8ba9cd608e90e6e114b22d7e7ca36b39
TsT <tst2005@gmail.com>
2022-11-25 14:59:03 +0100

    db_keepresult

--- lib/db_keepresult.jq.lib.sh
+++ lib/db_keepresult.jq.lib.sh
@@ -2,3 +2,4 @@ jq_deps_db_keepresult=''
 jq_function_db_keepresult='def db_keepresult($resultfield;$dbfield;$idfield):
 	(.[$resultfield]|map({key:., value:true})|from_entries) as $want | .[$dbfield] |= map(select($want[.[$idfield]]))|del(.[$resultfield]);
 def db_keepresult: db_keepresult("result";"db";"id");'
+jq_function_db_keepresult="$(cat "${dir:-.}/jq/db_keepresult.jq")"
